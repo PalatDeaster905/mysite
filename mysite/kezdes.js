ff86a5d0385bf43899d659c98cecdd9d94939355
PalatDeaster905 <ppragai49@gmail.com>
2024-09-28 13:41:32 +0200

    Add files via upload

---
+++ mysite/kezdes.js
@@ -0,0 +1,57 @@
+function showelements() {
+
+    var element1 = document.getElementById("hidden");
+
+    var element2 = document.getElementById("hidden2");
+
+    if (element1.style.display === "none" && element2.style.display === "none") {
+        element1.style.display = "block";
+        element2.style.display = "block";
+    } else {
+        element1.style.display = "none";
+        element2.style.display = "none";
+    }
+}
+
+function showelements2() {
+
+    var element1 = document.getElementById("alhidden");
+
+    var element2 = document.getElementById("alhidden2");
+
+    var element3 = document.getElementById("alhidden3");
+
+    var element4 = document.getElementById("alhidden4");
+
+    if (element1.style.display === "none" && element2.style.display === "none") {
+        element1.style.display = "block";
+        element2.style.display = "block";
+        element3.style.display = "block";
+        element4.style.display = "block";
+    } else {
+        element1.style.display = "none";
+        element2.style.display = "none";
+        element3.style.display = "none";
+        element4.style.display = "none";
+    }
+}
+
+
+function showelements3() {
+
+    var element1 = document.getElementById("socials");
+
+    var element2 = document.getElementById("socials2");
+
+    var element3 = document.getElementById("socials3");
+
+    if (element1.style.display === "none" && element2.style.display === "none") {
+        element1.style.display = "block";
+        element2.style.display = "block";
+        element3.style.display = "block";
+    } else {
+        element1.style.display = "none";
+        element2.style.display = "none";
+        element3.style.display = "none";
+    }
+}
